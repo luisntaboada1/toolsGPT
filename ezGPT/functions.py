8a0fcc48ae40f1bf994f81fdb4b029f28097e223
luisntaboada1 <145000472+luisntaboada1@users.noreply.github.com>
2024-02-03 13:28:11 -0600

    Update functions.py

--- ezGPT/functions.py
+++ ezGPT/functions.py
@@ -4,11 +4,10 @@ from openai import OpenAI
 
 client = OpenAI(api_key = openai_api_key)
 
-history = [{"role": "system", "content": "You are a helpful assistant."}]
+#history = [{"role": "system", "content": "You are a helpful assistant."}]
 
 def basic_chat(user_input, chat_history, gpt_model):
 
-    
     chat_history.append({"role": "user", "content": user_input})
 
     completion = client.chat.completions.create(
@@ -19,7 +18,10 @@ def basic_chat(user_input, chat_history, gpt_model):
     chat_history.append({"role": "assistant", "content": completion.choices[0].message.content})
     print(completion.choices[0].message.content)
     print('----------------------------------------')
+    return_history = chat_history
     #print(chat_history)
     #print('----------------------------------------')
-    return chat_history
+    return return_history
+
+
 
