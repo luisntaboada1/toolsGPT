b5894e2fe455dd4f7547d3d91865b1b9cf53524a
luisntaboada1 <145000472+luisntaboada1@users.noreply.github.com>
2024-02-02 20:14:34 -0600

    Add files via upload

---
+++ ezGPT/functions.py
@@ -0,0 +1,25 @@
+
+from keys import *
+from openai import OpenAI
+
+client = OpenAI(api_key = openai_api_key)
+
+history = [{"role": "system", "content": "You are a helpful assistant."}]
+
+def basic_chat(user_input, chat_history, gpt_model):
+
+    
+    chat_history.append({"role": "user", "content": user_input})
+
+    completion = client.chat.completions.create(
+        model=gpt_model,
+        messages=chat_history
+    )
+
+    chat_history.append({"role": "assistant", "content": completion.choices[0].message.content})
+    print(completion.choices[0].message.content)
+    print('----------------------------------------')
+    #print(chat_history)
+    #print('----------------------------------------')
+    return chat_history
+
---
+++ ezGPT/use_examples.py
@@ -0,0 +1,10 @@
+import functions
+from functions import chat_history
+
+chat_history = history = [{"role": "system", "content": "You are a helpful assistant."}]
+
+while True: 
+    message = input('Input: ')
+    functions.basic_chat(message, history, "gpt-3.5-turbo")
+    chat_history = functions.basic_chat
+    continue
